8094320f6ffc24b67f4fbe0973a0f9ead65d92f8
Daniil <daniil.shapovalov200247@gmail.com>
2023-04-21 22:04:31 +0300

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Shopify
+
+Shopify app super easy app
+Follow The Given steps to run the web app
+
+1. git clone this github
+2. Install Node (use brew install for Mac)
+3. Open terminal and type in locate to the git directory and type in <code>npm i</code>
+4. In case of api usage. Use mongod
+5. run project locally <code>node server.js</code>
+6. go to http://localhost:4000
---
+++ public/home/controllers/main.controller.js
@@ -0,0 +1,22 @@
+(function(){
+    angular
+        .module("Shopify")
+        .controller("MainController", MainController);
+
+    function MainController() {
+        var vm = this;
+        vm.checkemail = checkemail;
+
+        function checkemail(email) {
+            if (email == undefined) {
+                vm.error = 'Please enter a valid email address';
+                vm.message = undefined
+            }
+            else {
+                vm.message = "You are now subscribed to LALALA SHOP Mailing List"
+                vm.error = undefined
+            }
+        }
+
+    }
+})();
